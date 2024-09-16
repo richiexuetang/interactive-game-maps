2968635f1527ec3c6c94d9b88000cc70c504b7c6
richiexue <richard772794659@gmail.com>
2024-09-16 05:21:59 -0700

    remove material iu icon lib

--- apps/ui/src/components/main-nav.tsx
+++ apps/ui/src/components/main-nav.tsx
@@ -5,31 +5,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { ZapIcon } from "./icons/zap-icon";
 import Link from "next/link";
 
-const pages = ["Products", "Pricing", "Blog"];
-
 export const MainNav = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -59,13 +39,12 @@ export const MainNav = () => {
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleOpenNavMenu}
               color="inherit"
             >
               <ZapIcon />
             </IconButton>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <ZapIcon />
           <Typography
             variant="h5"
             noWrap
