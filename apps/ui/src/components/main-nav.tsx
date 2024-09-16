f9c21138c886bae6b6df19bc9f8c98042e2797a2
richiexue <richard772794659@gmail.com>
2024-09-16 07:34:08 -0700

    hydration error resolve

--- apps/ui/src/components/main-nav.tsx
+++ apps/ui/src/components/main-nav.tsx
@@ -19,7 +19,7 @@ export const MainNav = () => {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mx: 2,
               display: { xs: "none", md: "flex" },
@@ -30,7 +30,7 @@ export const MainNav = () => {
               textDecoration: "none",
             }}
           >
-            <Link href="/">Ritcher Map</Link>
+            Ritcher Map
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
