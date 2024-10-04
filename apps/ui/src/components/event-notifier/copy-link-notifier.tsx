a9d1c560e72ae326b8dab547e99e6745c1f4067a
richiexue <richard772794659@gmail.com>
2024-10-03 20:10:42 -0700

    add https prefix

--- apps/ui/src/components/event-notifier/copy-link-notifier.tsx
+++ apps/ui/src/components/event-notifier/copy-link-notifier.tsx
@@ -22,7 +22,7 @@ export const CopyLinkNotifier = () => {
   const mapEvents = useMapEvents({
     contextmenu: () => {
       const center = mapEvents.getCenter();
-      const url = `${process.env.VERCEL_URL}map/${params.slug}?lat=${
+      const url = `https://${process.env.VERCEL_URL}map/${params.slug}?lat=${
         center.lat
       }&lng=${center.lng}&zoom=${mapEvents.getZoom()}`;
       copy(url).then(() => {});
--- apps/ui/src/lib/graphql/apollo-client.ts
+++ apps/ui/src/lib/graphql/apollo-client.ts
@@ -9,7 +9,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache({}),
     link: new HttpLink({
-      uri: `${process.env.VERCEL_URL}/api/graphql`,
+      uri: `https://${process.env.VERCEL_URL}/api/graphql`,
     }),
   });
 });
