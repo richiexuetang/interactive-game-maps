626265547c46e572a19b9e7ae8e46dd4eee8d4ce
richiexue <richard772794659@gmail.com>
2024-10-03 22:08:41 -0700

    Revert "update api url"
    This reverts commit 0c62bde09d4f9e563ef4f189fc1a5b472c36f2f9.

--- apps/ui/codegen.ts
+++ apps/ui/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/graphql`,
+  schema: "http://localhost:3000/api/graphql",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ["src/**/*.{ts,tsx}"],
   generates: {
