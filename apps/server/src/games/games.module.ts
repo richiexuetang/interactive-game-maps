b57b61dbcc930a320f9dd4ff947893f5a117bc44
richiexue <richard772794659@gmail.com>
2024-10-03 18:36:38 -0700

    clean up

--- apps/server/src/games/games.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { GamesResolver } from "./games.resolver";
-
-@Module({
-  imports: [],
-  providers: [GamesResolver],
-})
-export class GamesModule {}
--- apps/server/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { UsersResolver } from "./users.resolver";
-import { UsersService } from "./users.service";
-
-@Module({
-  imports: [],
-  providers: [UsersResolver, UsersService],
-})
-export class UsersModule {}
