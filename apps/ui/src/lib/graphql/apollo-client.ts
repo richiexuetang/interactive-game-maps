882012b8294d9dfff7cf9f95a267ca8e65a22508
richiexue <richard772794659@gmail.com>
2024-10-03 22:08:06 -0700

    Revert "use system variables"
    This reverts commit 6d638b49bf978d35d0ae12c0e4a83c94c9fd17c8.

--- apps/ui/src/lib/graphql/apollo-client.ts
+++ apps/ui/src/lib/graphql/apollo-client.ts
@@ -9,7 +9,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache({}),
     link: new HttpLink({
-      uri: `${process.env.VERCEL_URL}/api/graphql`,
+      uri: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/graphql`,
     }),
   });
 });
