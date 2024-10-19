4473e090afc34736f8bff490168d5bc2bab407d1
richiexue <richard772794659@gmail.com>
2024-10-19 09:12:14 -0700

    perf: replace x-data-grid with mrt for smaller bundle size

--- apps/ui/src/components/data-grid/checklist-grid.tsx
+++ apps/ui/src/components/data-grid/checklist-grid.tsx
@@ -1,63 +1,100 @@
 "use client";
 
 import { Checkbox, FormControlLabel } from "@mui/material";
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from "material-react-table";
 import * as React from "react";
 import { titleCase } from "@/lib/utils";
 import { useAuthStore } from "@/store";
-import { MarkerFoundCheckbox } from "../fields/marker-found-checkbox";
 
+type Checklist = {
+  found: number;
+  category: {
+    title: string;
+  };
+  title: string;
+  mapSlug: string;
+  description: string;
+};
 interface ChecklistGridProps {
   locations: any;
 }
 export const ChecklistGrid = ({ locations }: ChecklistGridProps) => {
   const user = useAuthStore((state) => state.user);
+
+  const cols = React.useMemo<MRT_ColumnDef<Checklist>[]>(
+    () => [
+      // {
+      //   accessorFn: (row) =>
+      //     user?.foundMarkers.map((m) => m.id).includes(row.found),
+      //   // accessorKey: "id",
+      //   header: "Found",
+      //   size: 150,
+      //   Cell: ({ renderedCellValue, row }) => <>{renderedCellValue}</>,
+      // },
+      {
+        accessorKey: "category.title",
+        header: "Category",
+        size: 150,
+      },
+      {
+        accessorKey: "title",
+        header: "Title",
+        size: 200,
+      },
+      {
+        accessorKey: "mapSlug",
+        header: "Map",
+        size: 150,
+      },
+      {
+        accessorKey: "description",
+        header: "Info",
+        size: 150,
+        Cell(props) {
+          return (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: props.renderedCellValue ?? "",
+              }}
+            />
+          );
+        },
+      },
+    ],
+    []
+  );
+
   const [hideFound, setHideFound] = React.useState(false);
 
   const filteredLocations = locations.filter((location: any) =>
     hideFound ? !found.map((m) => m.id).includes(location.id as number) : true
   );
-  if (!locations) return null;
-
-  const columns: GridColDef[] = [
-    {
-      field: "found",
-      headerName: "Found",
-      display: "flex",
-      renderCell: (params) => {
-        return (
-          <MarkerFoundCheckbox markerId={parseInt(params.id.toString())} />
-        );
-      },
-      editable: true,
-      width: 75,
-      type: "boolean",
-    },
-    { field: "categoryId", headerName: "Category", width: 100 },
-    { field: "title", headerName: "Name", width: 250 },
-    { field: "mapSlug", headerName: "Map", width: 150 },
-    {
-      field: "description",
-      headerName: "Info",
-      width: 650,
-      renderCell(params) {
-        return <div dangerouslySetInnerHTML={{ __html: params.value }} />;
-      },
-    },
-  ];
 
   const found = user?.foundMarkers ?? [];
 
-  const rows: GridRowsProp = filteredLocations.map(
+  const data: Checklist[] = filteredLocations.map(
     ({ id, title, category, mapSlug, description }: any) => ({
       id,
       found: found.map((m) => m.id).includes(id as number),
       title,
-      categoryId: category?.title,
+      category: {
+        title: category?.title,
+      },
       mapSlug: titleCase(mapSlug!.replaceAll("-", " ")),
       description,
     })
   )!;
+
+  const table = useMaterialReactTable({
+    columns: cols,
+    data,
+  });
+
+  if (!locations) return null;
 
   return (
     <div className="w-full h-full">
@@ -71,7 +108,7 @@ export const ChecklistGrid = ({ locations }: ChecklistGridProps) => {
           }
           label="Hide Found Items"
         />
-        <DataGrid rows={rows} columns={columns} />
+        <MaterialReactTable table={table} />
       </div>
     </div>
   );
